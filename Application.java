08fba1ff5d1422a398fce66178331da756000095
Guillem RM <guillem.rm.25@gmail.com>
2024-04-28 23:48:59 +0200

    Casi todo hecho, faltan explicaciones de instalación y uso

--- Application.java
+++ Application.java
@@ -28,13 +28,14 @@ public class Application {
     public static void main(String[] args) throws InterruptedException {
         System.out.println();
 
-        // Input con la tupla del NFA
+        // Pedir tupla del NFA
         System.out.println("Introduce el NFA, siguiendo el siguiente esquema:");
         System.out.println(PURPLE + "\t({q0, q1...}, {c0, c1...), {(qi, ci) = {qi, qy...}...}, qi, {qi, qy...})" + RESET);
         System.out.println("\tPara las transiciones nulas utiliza este simbolo: " + PURPLE + "ε" + RESET);
         String input = scanner.nextLine();
         CharStream inputStream = CharStreams.fromString(input);
         
+        // Inicializar Lexer y Parser
         NFALexer lexer = new NFALexer(inputStream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         NFAParser parser = new NFAParser(tokens);
@@ -59,18 +60,22 @@ public class Application {
         // Caminar por el árbol con el listener para ir obteniendo los datos del NFA
         ParseTreeWalker walker = new ParseTreeWalker();
         walker.walk(listener, tree);
-
         obtenerDatosNFA(listener);
 
+        // Generar código LaTex a partir de los datos del NFA y crear fichero .tex
         String latexCode = generateLatexCode();
-
         try (FileWriter writer = new FileWriter(title + ".tex")) {
             writer.write(latexCode);
         } catch (IOException e) {}
 
+        // Convertir el .tex en un .pdf (compilar con pdflatex)
         convertLatexToPdf(title + ".tex");
     }
 
+    /**
+     * Función para guardar todos los datos del NFA usando las funciones del NFABaseListener
+     * @param listener
+     */
     public static void obtenerDatosNFA(NFABaseListener listener) {
         // Pedir nombre del NFA, que será el nombre del PDF
         System.out.print("\nIntroduce el nombre del NFA (sin carácteres especiales): ");
@@ -96,6 +101,10 @@ public class Application {
         System.out.println("\tEstados finales " + PURPLE + finals + RESET);
     }
     
+    /**
+     * Función para generar todo el código del fichero .tex que dibujará el NFA
+     * @return String
+     */
     public static String generateLatexCode() {
         StringBuilder latexCode = new StringBuilder();
         
@@ -105,7 +114,7 @@ public class Application {
         latexCode.append("\\usetikzlibrary{automata, positioning, arrows}\n");
         latexCode.append("\\DeclareUnicodeCharacter{03B5}{\\ensuremath{\\epsilon}}\n");
         latexCode.append("\\definecolor{urvred}{RGB}{143,42,43}\n");
-        latexCode.append("\\tikzset{->, >=stealth, node distance=3cm, every state/.style={thick, fill=urvred!30}, initial text=$ $}\n");
+        latexCode.append("\\tikzset{->, >=stealth, node distance=2.8cm, every state/.style={thick, fill=urvred!30}, initial text=$ $}\n");
         latexCode.append("\\begin{document}\n");
         latexCode.append("\\center \\LARGE \\textbf{" + title + "}\n");
         latexCode.append("\\vspace{0.2cm}\n");
@@ -127,26 +136,46 @@ public class Application {
 
         // Agregar estados
         String ant = null;
+        int i = 0;
+        boolean right = true;
         for (String state : states) {
+            String pos = "";
+            if (i != 0 && i % 2 != 0) {
+                if (right) {
+                    pos = "right of=" + ant;
+                }
+                else pos = "left of=" + ant;
+                right = !right;
+            } else {
+                if (i % 4 == 2) pos = "below of=" + ant + ", xshift=1.5cm";
+                else if (i % 4 == 0) pos = "below of=" + ant + ", xshift=-1.5cm";
+            }
             latexCode.append("      \\node[state");
             if (initial.equals(state)) latexCode.append(",initial");
             if (finals.contains(state)) latexCode.append(",accepting");
-            if (ant != null) latexCode.append(",right of=" + ant);
+            if (ant != null) latexCode.append("," + pos);
             latexCode.append("] (" + state + ") {" + state + "};\n");
             ant = state;
+            i++;
         }
 
-        // Agregar transiciones
+        // Agregar relaciones
         latexCode.append("      \\draw");
         for (HashMap.Entry<HashMap<String, String>, Set<String>> entry : relations.entrySet()) {
             HashMap<String, String> relation = entry.getKey();
             Set<String> destinationStates = entry.getValue();
             String originState = relation.keySet().iterator().next();
             String symbol = relation.get(originState);
+            i = states.indexOf(originState);
             for (String destinationState : destinationStates) {
                 String edge = "";
-                if (originState.equals(destinationState)) edge = "loop above";
-                else edge = "bend left, above";
+                if (originState.equals(destinationState)) {
+                    if (i >= 2 && i % 2 == 0) edge = "loop right";
+                    else if (i >= 2 && i % 2 != 0) edge = "loop left";
+                    else edge = "loop above";
+                } else {
+                    edge = "bend left, above";
+                }
                 latexCode.append("\n             (" + originState + ") edge[" + edge + "] node{" + symbol + "} (" + destinationState + ")");
             }
         }
@@ -160,26 +189,50 @@ public class Application {
         return latexCode.toString();
     }
 
+    /**
+     * Función para obtener el PDF a partir del fichero .tex
+     * @param latexFile
+     * @throws InterruptedException
+     */
     public static void convertLatexToPdf(String latexFile) throws InterruptedException {
         System.out.println("\nGenerando archivo PDF...");
         Thread.sleep(1000);
+
+        // Crear Thread para la ejecución del pdflatex
+        Thread latexThread = new Thread(() -> {
+            try {
+                // Ejecutar pdflatex para convertir el archivo .tex en .pdf
+                ProcessBuilder builder = new ProcessBuilder("pdflatex", latexFile);
+                Process process = builder.start();
+    
+                // Esperar a que el proceso termine
+                if (process.waitFor() == 0) {
+                    System.out.println(GREEN + "El archivo PDF se ha generado correctamente\n" + RESET);
+                } else {
+                    System.out.println(RED + "Se ha producido un error al generar el archivo PDF\n" + RESET);
+                }
+            } catch (IOException e) { 
+                System.out.println(RED + "Ha surgido un error en el proceso\n" + RESET);
+            } catch (InterruptedException e) {}
+        });
+
+        // Iniciar ejecución del Thread
+        latexThread.start();
+        // Intentar interrumpir la ejecución pasados 5 segundos
+        latexThread.join(5000);
+
+        // Si no se ha podido interrumpir significa que se ha quedado pillado en el pdflatex, y por lo tanto, hay algun error en el NFA
+        if (latexThread.isAlive()) {
+            System.out.println(RED + "El proceso está tardando más de lo esperado o ha habido algun error. Comprueba que no hayan relaciones con estados inexistentes u otros problemas similares\n" + RESET);
+            latexThread.interrupt();
+        }
+
+        // Eliminar ficheros auxiliares
+        ProcessBuilder builder = new ProcessBuilder("bash", "-c", "rm *.aux *.log *.tex");
         try {
-            // Ejecutar pdflatex para convertir el archivo .tex en .pdf
-            ProcessBuilder builder = new ProcessBuilder("pdflatex", latexFile);
-            Process process = builder.start();
-
-            // Esperar a que el proceso termine
-            if (process.waitFor() == 0) {
-                System.out.println(GREEN + "El archivo PDF se ha generado correctamente\n" + RESET);
-            } else {
-                System.out.println(RED + "Se ha producido un error al generar el archivo PDF\n" + RESET);
-            }
-
-            // Eliminar ficheros auxiliares
-            builder = new ProcessBuilder("bash", "-c", "rm *.aux *.log");
             builder.start();
-        } catch (IOException | InterruptedException e) {
-            System.out.println(RED + "Ha surgido un error en el proceso\n" + RESET);
+        } catch (IOException e) {
+            System.out.println(RED + "Algo ha salido mal al intentar eliminar los ficheros auxiliares (.aux .log .tex)" + RESET);
         }
     }
 
